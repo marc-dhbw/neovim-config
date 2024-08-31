04ed2e1c2b57ab5e28cbba090ebfd3904d6fc315
marc <schoenigm.tin23@student.dhbw-heidenheim.de>
2024-08-31 14:43:00 +0200

    rebuilded config

--- init.lua
+++ init.lua
@@ -1,10 +1,1 @@
--- Load core settings
-require("core.options")
-require("core.keymaps")
-require("core.autocmds")
-
--- Load utilities
-require("utils.utils")
-
--- Load plugins
-require("plugins") -- Set <space> as the leader key
+require("core")
