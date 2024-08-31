8888ef205e28338dc8413e1b6b5fa3f0a57a0004
marc <schoenigm.tin23@student.dhbw-heidenheim.de>
2024-08-31 17:51:15 +0200

    changed c compiling to .exe

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -68,7 +68,7 @@ function CompileAndRunC()
         end
 
         -- Create the output file path
-        local output_file = current_dir .. "/a.out"
+        local output_file = current_dir .. "/a.exe"
 
         -- Create the compile command by joining all found C files
         local compile_command = "zig cc -o " .. output_file .. " " .. table.concat(c_files, " ")
