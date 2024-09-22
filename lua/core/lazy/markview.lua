f48e1f67c09566f904576d2bf5cccb61ed33b44f
marc-dhbw <schoenigm.tin23@student.dhbw-heidenheim.de>
2024-09-22 15:48:23 +0200

    added keymap to toggle markview

--- lua/core/lazy/markview.lua
+++ lua/core/lazy/markview.lua
@@ -7,5 +7,13 @@ return {
             theme = 'dark',  -- Set theme to 'light' or 'dark'
             auto_start = true,  -- Automatically start the preview on entering a markdown file
         })
+        -- Add keymap to toggle splitview with leader+tm
+        vim.api.nvim_set_keymap(
+            'n',
+            '<leader>tm',
+            ':Markview splitToggle<CR>',
+            { noremap = true, silent = true }
+        )
+
     end,
 }
